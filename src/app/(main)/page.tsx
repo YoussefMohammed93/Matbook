57bc63178a4086f7c84ac59535303a912f555873
Youssef Mohammed <youssefmohammed2093@gmail.com>
2024-10-26 15:04:37 +0300

    Trends sidebar + people you may know

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
 import Post from "@/components/posts/post";
 import { postDataInclude } from "@/lib/types";
+import TrendsSidebar from "@/components/trends-sidebar";
 import PostEditor from "@/components/posts/editor/post-editor";
 
 export default async function Home() {
@@ -10,13 +11,14 @@ export default async function Home() {
   });
 
   return (
-    <main className="w-full min-w-0">
+    <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
+      <TrendsSidebar />
     </main>
   );
 }
