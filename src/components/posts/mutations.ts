b5d178da1d4f616dc2eacd9cf18a5997a94971d3
Youssef Mohammed <youssefmohammed2093@gmail.com>
2024-10-26 15:39:40 +0300

    Deleting posts

---
+++ src/components/posts/mutations.ts
@@ -0,0 +1,60 @@
+import {
+  InfiniteData,
+  QueryFilters,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { deletePost } from "./actions";
+import { PostsPage } from "@/lib/types";
+import { useToast } from "@/hooks/use-toast";
+import { usePathname, useRouter } from "next/navigation";
+
+export function useDeletePostMutation() {
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const mutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: async (deletedPost) => {
+      const queryFilter: QueryFilters = { queryKey: ["post-feed"] };
+
+      await queryClient.cancelQueries(queryFilter);
+
+      queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
+        queryFilter,
+        (oldData) => {
+          if (!oldData) return;
+
+          return {
+            pageParams: oldData.pageParams,
+            pages: oldData.pages.map((page) => ({
+              nextCursor: page.nextCursor,
+              posts: page.posts.filter((p) => p.id !== deletedPost.id),
+            })),
+          };
+        }
+      );
+
+      toast({
+        description: "This post has been successfully deleted.",
+      });
+
+      if (pathname === `/posts/${deletedPost.id}`) {
+        router.push(`/users/${deletedPost.user.username}`);
+      }
+    },
+    onError(error) {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Failed to delete post. Please try again.",
+      });
+    },
+  });
+
+  return mutation;
+}
--- src/components/posts/post.tsx
+++ src/components/posts/post.tsx
@@ -1,33 +1,47 @@
+"use client";
+
 import Link from "next/link";
 import { PostData } from "@/lib/types";
 import UserAvatar from "../user-avatar";
+import PostMoreButton from "./post-more-button";
 import { formatRelativeDate } from "@/lib/utils";
+import { useSession } from "@/app/(main)/session-provider";
 
 interface PostProps {
   post: PostData;
 }
 
 export default function Post({ post }: PostProps) {
+  const { user } = useSession();
+
   return (
-    <article className="space-y-3 rounded-sm border bg-card p-5 shadow-sm">
-      <div className="flex flex-wrap gap-3">
-        <Link href={`/users/${post.user.username}`}>
-          <UserAvatar avatarUrl={post.user.avatarUrl} />
-        </Link>
-        <div>
-          <Link
-            href={`/users/${post.user.username}`}
-            className="block font-medium hover:underline"
-          >
-            {post.user.displayName}
+    <article className="group/post space-y-3 rounded-sm border bg-card p-5 shadow-sm">
+      <div className="flex justify-between gap-3">
+        <div className="flex flex-wrap gap-3">
+          <Link href={`/users/${post.user.username}`}>
+            <UserAvatar avatarUrl={post.user.avatarUrl} />
           </Link>
-          <Link
-            href={`/posts/${post.id}`}
-            className="block text-sm text-muted-foreground hover:underline"
-          >
-            {formatRelativeDate(post.createdAt)}
-          </Link>
+          <div>
+            <Link
+              href={`/users/${post.user.username}`}
+              className="block font-medium hover:underline"
+            >
+              {post.user.displayName}
+            </Link>
+            <Link
+              href={`/posts/${post.id}`}
+              className="block text-sm text-muted-foreground hover:underline"
+            >
+              {formatRelativeDate(post.createdAt)}
+            </Link>
+          </div>
         </div>
+        {post.user.id === user.id && (
+          <PostMoreButton
+            post={post}
+            className="opacity-0 transition-opacity group-hover/post:opacity-100"
+          />
+        )}
       </div>
       <div className="whitespace-pre-line break-words">{post.content}</div>
     </article>
