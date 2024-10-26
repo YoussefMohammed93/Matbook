98cbe0055ee44a0023e00fd9df572004db1f7dbf
Youssef Mohammed <youssefmohammed2093@gmail.com>
2024-10-26 15:23:11 +0300

    Use infinite query

---
+++ src/components/posts/posts-loading-skeleton.tsx
@@ -0,0 +1,26 @@
+import { Skeleton } from "../ui/skeleton";
+
+export default function PostsLoadingSkeleton() {
+  return (
+    <div className="space-y-5">
+      <PostLoadingSkeleton />
+      <PostLoadingSkeleton />
+      <PostLoadingSkeleton />
+    </div>
+  );
+}
+
+function PostLoadingSkeleton() {
+  return (
+    <div className="w-full animate-pulse space-y-3 rounded-sm border bg-card p-5 shadow-sm">
+      <div className="flex flex-wrap gap-3">
+        <Skeleton className="size-12 rounded-sm border" />
+        <div className="space-y-1.5">
+          <Skeleton className="h-4 w-24 rounded-sm border" />
+          <Skeleton className="h-4 w-20 rounded-sm border" />
+        </div>
+      </div>
+      <Skeleton className="h-16 rounded-sm border" />
+    </div>
+  );
+}
